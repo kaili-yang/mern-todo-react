2051b54ac3a3331a73dbd55ad4334509851c144e
kaili-yang <124ykl@gmail.com>
2023-04-06 00:00:37 -0700

    update: backend build bug

---
+++ server/webpack.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+ mode: 'development',
+ entry: __dirname + "/server.js", // 唯一入口文件
+ output: {
+     path: __dirname + "/dist", // 打包后的文件存放的路径
+     filename: "bundle.js" // 打包后输出文件的文件名
+ },
+ module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        } 
+      }
+    },
+  ]
+}
+};
